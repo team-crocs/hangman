6ceea1e5e9be2b2a31a858bb67720f53f158937f
alexchao26 <alexchaoo@gmail.com>
2019-11-24 13:45:30 -0500

    default container removed

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './styles/App.css';
 import './styles/AppLg.css';
 import { Route } from 'react-router-dom';
-import regeneratorRuntime from 'regenerator-runtime';
-import DefaultContainer from './containers/DefaultContainer';
+import regeneratorRuntime from 'regenerator-runtime'; // for async/await in React
+import GameRoom from './containers/GameRoom';
 
 const App = () => (
-  <Route path="/" component={DefaultContainer} />
+  <Route path="/" component={GameRoom} />
 );
 
 export default App;
