6dcaa6f25b7425ecd3a32b99072197efc7557ab3
alexchao26 <alexchaoo@gmail.com>
2019-11-24 13:41:23 -0500

    gameRoomReducers removed

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,13 +2,11 @@ import { combineReducers } from 'redux';
 
 // import reducers here
 import hangmanReducer from './hangmanReducers';
-import gameRoomsReducer from './gameRoomsReducers';
 
 // combine reducers
 const reducers = combineReducers({
-  // key value pairs of name of reducer catagory: imported name on line 3
+  // key value pairs of name of reducer catagory: imported above
   hangman: hangmanReducer,
-  rooms: gameRoomsReducer
 });
 
 export default reducers;
